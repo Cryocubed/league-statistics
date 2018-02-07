8697f8eb376b78cb47b2d7cc8a79d6025d2aa169
Joseph Baca <j.cryocubed@gmail.com>
2018-02-07 13:27:39 -0600

    Removed profiler

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-import cProfile
-
-from config import *
 from util import *
 
 
 def main():
-    # Initialization
-    api = RiotAPI.RiotAPI()
 
     # Your code here
     player_name = 'player'
     pstats = player_stats.compute_all_stats(api, player_name, season_list=[9, 10, 11])
     latex.build_champion_report(pstats, player_name)
 
-    # Closing code
-    api.shutdown()
 
 #########
-# Debugging
+# Initialization
+api = RiotAPI.RiotAPI()
 
+main()
 
-if DEBUG_MODE:
-    cProfile.run('main()', sort=1)
-else:
-    main()
+# Closing code
+api.shutdown()
